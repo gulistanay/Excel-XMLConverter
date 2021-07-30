91fddeffeea36cd875f8af241c1054f6718b98a5
Gülistan Ay <gulistanayy@gmail.com>
2021-07-30 11:47:56 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace FormXmlKullanimi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonTimer = new System.Windows.Forms.Button();
+            this.buttonyeniForm = new System.Windows.Forms.Button();
+            this.buttonTimerBaslat = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 51);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(748, 266);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonTimer
+            // 
+            this.buttonTimer.Location = new System.Drawing.Point(671, 421);
+            this.buttonTimer.Name = "buttonTimer";
+            this.buttonTimer.Size = new System.Drawing.Size(112, 35);
+            this.buttonTimer.TabIndex = 14;
+            this.buttonTimer.Text = "Timer Durdur";
+            this.buttonTimer.UseVisualStyleBackColor = true;
+            this.buttonTimer.Click += new System.EventHandler(this.buttonTimer_Click);
+            // 
+            // buttonyeniForm
+            // 
+            this.buttonyeniForm.Location = new System.Drawing.Point(35, 343);
+            this.buttonyeniForm.Name = "buttonyeniForm";
+            this.buttonyeniForm.Size = new System.Drawing.Size(175, 49);
+            this.buttonyeniForm.TabIndex = 15;
+            this.buttonyeniForm.Text = "Yeni Dosya Yükle";
+            this.buttonyeniForm.UseVisualStyleBackColor = true;
+            this.buttonyeniForm.Click += new System.EventHandler(this.buttonyeniForm_Click);
+            // 
+            // buttonTimerBaslat
+            // 
+            this.buttonTimerBaslat.Location = new System.Drawing.Point(671, 356);
+            this.buttonTimerBaslat.Name = "buttonTimerBaslat";
+            this.buttonTimerBaslat.Size = new System.Drawing.Size(112, 35);
+            this.buttonTimerBaslat.TabIndex = 17;
+            this.buttonTimerBaslat.Text = "Timer Başlat";
+            this.buttonTimerBaslat.UseVisualStyleBackColor = true;
+            this.buttonTimerBaslat.Click += new System.EventHandler(this.buttonTimerBaslat_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(518, 363);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(112, 22);
+            this.numericUpDown1.TabIndex = 18;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(555, 333);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Enabled = false;
+            this.label2.Location = new System.Drawing.Point(607, 333);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 17);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "dk";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(814, 480);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.buttonTimerBaslat);
+            this.Controls.Add(this.buttonyeniForm);
+            this.Controls.Add(this.buttonTimer);
+            this.Controls.Add(this.dataGridView1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Excel\'den Xml\'e Dönüşüm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonTimer;
+        private System.Windows.Forms.Button buttonyeniForm;
+        private System.Windows.Forms.Button buttonTimerBaslat;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ FormDosyaYukle.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using ExcelApp = Microsoft.Office.Interop.Excel;
+
+namespace FormXmlKullanimi
+{
+    public partial class FormDosyaYukle : Form
+    {
+        public FormDosyaYukle()
+        {
+            InitializeComponent();
+        }
+
+        DataTable dt;
+        private void buttonDosyaSec_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string DosyaYolu;
+                string DosyaAdi;
+
+                OpenFileDialog file = new OpenFileDialog();
+
+                file.Filter = "Excel Dosyası | *.xls; *.xlsx; *.xlsm";
+
+                if (file.ShowDialog() == DialogResult.OK)
+                {
+                    Cursor.Current = Cursors.WaitCursor;
+                    DosyaYolu = file.FileName;// seçilen dosyanın tüm yolunu verir
+                    DosyaAdi = file.SafeFileName;// seçilen dosyanın adını verir.
+                    ExcelApp.Application excelApp = new ExcelApp.Application();
+                    if (excelApp == null)
+                    {
+                        MessageBox.Show("Excel yüklü değil.");
+                        return;
+                    }
+                    //Excel Dosyası Açılıyor.
+                    ExcelApp.Workbook excelBook = excelApp.Workbooks.Open(DosyaYolu);
+                    //Excel Dosyasının Sayfası Seçilir.
+                    ExcelApp._Worksheet excelSheet = excelBook.Sheets[1];
+                    //Excel Dosyasının ne kadar satır ve sütun kaplıyorsa tüm alanları alır.
+                    ExcelApp.Range excelRange = excelSheet.UsedRange;
+                    int satirSayisi = excelRange.Rows.Count; //Sayfanın satır sayısını alır.
+                    int sutunSayisi = excelRange.Columns.Count;//Sayfanın sütun sayısını alır.
+                    dt = ToDataTable(excelRange, satirSayisi, sutunSayisi);
+
+                    dt.TableName = DosyaAdi.Split('.')[0]; //XML e kayıt
+
+                    dataGridView1.DataSource = dt;
+                    dataGridView1.Refresh();
+
+                    Cursor.Current = Cursors.Default;
+
+                    excelApp.Quit();
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+                }
+                else
+                {
+                    MessageBox.Show("Dosya Seçilemedi.");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Dosya okunamadı.");
+            }
+        }
+
+        public DataTable ToDataTable(ExcelApp.Range range, int rows, int cols)
+        {
+            DataTable table = new DataTable();
+            for (int i = 1; i <= rows; i++)
+            {
+                if (i == 1)
+                { // ilk satırı Sutun Adları olarak kullanıldığından
+                  // bunları Sutün Adları Olarak Kaydediyoruz.
+                    for (int j = 1; j <= cols; j++)
+                    {
+                        //Sütunların içeriği boş mu kontrolü yapılmaktadır.
+                        if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                            table.Columns.Add(range.Cells[i, j].Value2.ToString());
+                        else //Boş olduğunda Kaçınsı Sutünsa Adı veriliyor.
+                            table.Columns.Add(j.ToString() + ".Sütun");
+                    }
+                    continue;
+                }
+                //Yukarıda Sütunlar eklendi
+                // onun şemasına göre yeni bir satır oluşturuyoruz. 
+                //Okunan verileri yan yana sıralamak için
+                var yeniSatir = table.NewRow();
+                for (int j = 1; j <= cols; j++)
+                {
+                    //Sütunların içeriği boş mu kontrolü yapılmaktadır.
+                    if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                        yeniSatir[j - 1] = range.Cells[i, j].Value2.ToString();
+                    else // İçeriği boş hücrede hata vermesini önlemek için
+                        yeniSatir[j - 1] = String.Empty;
+                }
+                table.Rows.Add(yeniSatir);
+            }
+            return table;
+
+
+        }
+
+        private void buttonXmlKaydet_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "XML|*.xml";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+
+                try
+                {
+                    dt.WriteXml(sfd.FileName);
+                    if (MessageBox.Show("XML dosyası hazırlandı. Dosya açılsın mı?", "Onay", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        System.Diagnostics.Process.Start(sfd.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 AnaForm = new Form1();
+            AnaForm.Show();
+        }
+    }
+}
